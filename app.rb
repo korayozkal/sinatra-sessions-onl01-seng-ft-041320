7db85598508234ddfc803890c196e266faf61d24
Koray Ozkal <koryozkal@gmail.com>
2020-06-12 01:32:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,13 +10,13 @@ class App < Sinatra::Base
     content_type :txt
   end
 
-  get '/' do
-    "Welcome to Sinatra Sessions! In this lab, we will be learning about the general principles behind a sessions cookie. In order to proceed, let's go to the '/first_exercise' path."
-  end
+  #get '/' do
+   # "Welcome to Sinatra Sessions! In this lab, we will be learning about the general principles behind a sessions cookie. In order to proceed, let's go to the '/first_exercise' path."
+  #end
 
-  get '/first_exercise' do
-    "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, navigate to the '/set' path."
-  end
+  #get '/first_exercise' do
+   # "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, navigate to the '/set' path."
+  #end
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
@@ -28,18 +28,17 @@ class App < Sinatra::Base
   end
 
   get '/fetch' do
-  session[:foo] value: {session[:foo]}
+    "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab at '/second_exercise' "
   end
 
   get '/second_exercise' do
-    
     "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
     #set session id here
 
-    if session[:id] = 1
+    if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
     else
@@ -48,7 +47,7 @@ class App < Sinatra::Base
   end
 
   get '/fetch_session_id' do
-     session[:id] value: {session[:id]}
+    "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
   end
 
   get '/logout' do
