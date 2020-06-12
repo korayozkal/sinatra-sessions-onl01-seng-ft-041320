8962265442a11453464484fdf727ed8d3b697357
Koray Ozkal <koryozkal@gmail.com>
2020-06-12 01:06:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
-    if session[:foo] == 'hello'
+    if session[:foo] = 'hello'
       redirect '/fetch'
     else
       "Session value has not been set!"
@@ -32,13 +32,15 @@ class App < Sinatra::Base
   end
 
   get '/second_exercise' do
+    
+    @session = 
     "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
     #set session id here
 
-    if session[:id] == 1
+    if session[:id] = 1
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
     else
